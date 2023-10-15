18429bb0a042dfae66b041f8ac54f5d92b710bb3
Larry Wang <larrywong9898@gmail.com>
2023-10-15 00:44:42 -0500

    add personal info

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -37,9 +37,9 @@ const store = configureStore(
   },
   applyMiddleware(thunk)
 );
-store.subscribe(() => {
-  console.log(store.getState().userReducer.files);
-});
+// store.subscribe(() => {
+//   console.log(store.getState().userReducer.files);
+// });
 const persistor = persistStore(store);
 
 export { store, persistor };
