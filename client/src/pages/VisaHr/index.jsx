b29a770310c7808f94fc4409a530ae15a2211dd0
Yingshan Qu <quying333@gmail.com>
2023-10-25 20:08:05 -0700

    add pagination for visa hr

--- client/src/pages/VisaHr/index.jsx
+++ client/src/pages/VisaHr/index.jsx
@@ -4,45 +4,45 @@ import { Card, Space } from "antd";
 
 const VisaHr = () => {
   return (
-    <Card style={{ width: "50%", height: "600px" }}>
-      <Space
-        direction="vertical"
-        size="large"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: "10%",
+    // <Card style={{ width: "50%", height: "600px" }}>
+    <Space
+      direction="vertical"
+      size="large"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "10%",
+      }}
+    >
+      <Button
+        component={Link}
+        to="inprogress"
+        variant="contained"
+        sx={{
+          width: "150px",
+          height: "150px",
+          borderRadius: "50%",
+          textAlign: "center",
         }}
       >
-        <Button
-          component={Link}
-          to="inprogress"
-          variant="contained"
-          sx={{
-            width: "150px",
-            height: "150px",
-            borderRadius: "50%",
-            textAlign: "center",
-          }}
-        >
-          In progress Employees
-        </Button>
-        <Button
-          component={Link}
-          to="all"
-          variant="contained"
-          sx={{
-            width: "150px",
-            height: "150px",
-            borderRadius: "50%",
-            textAlign: "center",
-          }}
-        >
-          All Employees
-        </Button>
-      </Space>
-    </Card>
+        In progress Employees
+      </Button>
+      <Button
+        component={Link}
+        to="all"
+        variant="contained"
+        sx={{
+          width: "150px",
+          height: "150px",
+          borderRadius: "50%",
+          textAlign: "center",
+        }}
+      >
+        All Employees
+      </Button>
+    </Space>
+    // </Card>
   );
 };
 export default VisaHr;
